2035522e5f14817e7c7000bd659d39ce5c936ee7
RachhadiyaYash <yashpatel2462002@gmail.com>
2024-09-08 09:47:46 +0530

    add backend

--- src/data/techDetails.jsx
+++ src/data/techDetails.jsx
@@ -4,7 +4,7 @@ const techDetails = {
       <div className="grid grid-cols-2 sm:grid-cols-4 gap-6">
         <div className="flex flex-col items-center justify-center bg-white/30 backdrop-blur-lg p-4 rounded-lg shadow-lg">
           <img
-            className="w-20 h-20 object-contain"
+            className="w-16 h-16 object-contain"
             src="/TechnologyStack/angular-svgrepo-com.svg"
             alt="Angular"
           />
@@ -60,50 +60,34 @@ const techDetails = {
         <div className="flex flex-col items-center justify-center bg-white/30 backdrop-blur-lg p-4 rounded-lg shadow-lg">
           <img
             className="w-20 h-20 object-contain"
-            src="/TechnologyStack/angular-svgrepo-com.svg"
-            alt="Angular"
+            src="/TechnologyStack/django-svgrepo-com.svg"
+            alt="dJango"
           />
-          <p className="text-center mt-2">Angular</p>
+          <p className="text-center mt-2">Django</p>
         </div>
         <div className="flex flex-col items-center justify-center bg-white/30 backdrop-blur-lg p-4 rounded-lg shadow-lg">
           <img
             className="w-16 h-16 object-contain"
-            src="/TechnologyStack/js-official-svgrepo-com.svg"
+            src="/TechnologyStack/laravel-svgrepo-com.svg"
             alt="JavaScript"
           />
-          <p className="text-center mt-2">JavaScript</p>
+          <p className="text-center mt-2">Laravel</p>
         </div>
         <div className="flex flex-col items-center justify-center bg-white/30 backdrop-blur-lg p-4 rounded-lg shadow-lg">
           <img
             className="w-16 h-16 object-contain"
-            src="/TechnologyStack/next-js-svgrepo-com.svg"
+            src="/TechnologyStack/node-js-svgrepo-com.svg"
             alt="Next.js"
           />
-          <p className="text-center mt-2">Next.js</p>
+          <p className="text-center mt-2">Node JS</p>
         </div>
         <div className="flex flex-col items-center justify-center bg-white/30 backdrop-blur-lg p-4 rounded-lg shadow-lg">
           <img
             className="w-16 h-16 object-contain"
-            src="/TechnologyStack/react-javascript-js-framework-facebook-svgrepo-com.svg"
+            src="/TechnologyStack/python-svgrepo-com.svg"
             alt="React"
           />
-          <p className="text-center mt-2">React</p>
-        </div>
-        <div className="flex flex-col items-center justify-center bg-white/30 backdrop-blur-lg p-4 rounded-lg shadow-lg">
-          <img
-            className="w-16 h-16 object-contain"
-            src="/TechnologyStack/typescript-svgrepo-com.svg"
-            alt="TypeScript"
-          />
-          <p className="text-center mt-2">TypeScript</p>
-        </div>
-        <div className="flex flex-col items-center justify-center bg-white/30 backdrop-blur-lg p-4 rounded-lg shadow-lg">
-          <img
-            className="w-16 h-16 object-contain"
-            src="/TechnologyStack/vue-dot-js-svgrepo-com.svg"
-            alt="Vue.js"
-          />
-          <p className="text-center mt-2">Vue.js</p>
+          <p className="text-center mt-2">Python</p>
         </div>
       </div>
     </div>
